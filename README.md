edfaa21ff28bb5bcfd6304de064a27929963c8c7
Ryan McNair <ryan.mcnair@gmail.com>
2020-10-05 12:22:11 -0500

    created doodler and platforms, added jump and platform moving

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # doodle-jump
+
+Follow along coding practice from freeCodeCamp.org
+
+https://youtu.be/8xPsg6yv7TU
---
+++ app.js
@@ -0,0 +1,94 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    const doodler = document.createElement('div')
+    let doodlerLeftSpace = 50
+    let doodlerBottomSpace = 150
+    let isGamerOver = false
+    let platformCount = 5
+    let platforms = []
+    let upTimerId 
+
+    function createDoodler() {
+        grid.appendChild(doodler)
+        doodler.classList.add('doodler')
+        doodlerLeftSpace = platforms[0].left
+        doodler.style.left = doodlerLeftSpace + 'px'
+        doodler.style.bottom = doodlerBottomSpace + 'px'
+    }
+    
+    class Platform {
+        constructor(newPlatBottom) {
+            this.bottom = newPlatBottom
+            this.left = Math.random() * 315
+            this.visual = document.createElement('div')
+
+            const visual = this.visual
+            visual.classList.add('platform')
+            visual.style.left = this.left + 'px'
+            visual.style.bottom = this.bottom + 'px'
+            grid.appendChild(visual)
+        }
+    }
+
+    function createPlatforms() {
+        for (let i = 0; i < platformCount; i++){
+            let platGap = 600 / platformCount
+            let newPlatBottom = 100 + i * platGap
+            let newPlatform = new Platform(newPlatBottom)
+            platforms.push(newPlatform)
+            console.log(platforms)
+        }
+    }
+
+    function movePlatforms() {
+        if(doodlerBottomSpace > 200){
+            platforms.forEach(platform => {
+                platform.bottom -= 4
+                let visual = platform.visual
+                visual.style.bottom = platform.bottom + 'px'
+            })
+        }
+    }
+
+    function jump() {
+        clearInterval(downTimerId)
+        upTimerId = setInterval(function() {
+            doodlerBottomSpace += 20
+            doodler.style.bottom = doodlerBottomSpace + 'px' 
+            if (doodlerBottomSpace > 350){
+                fall()
+            }
+        },30)
+    }
+
+    function fall (){
+        clearInterval(upTimerId)
+        console.log(upTimerId)
+        downTimerId = setInterval(function() {
+            doodlerBottomSpace -= 5
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace <= 0){
+                gameOver()
+            }
+        },30)
+    }
+
+    function gameOver() {
+        console.log('game over')
+        isGamerOver = true
+        clearInterval(upTimerId)
+        clearInterval(downTimerId)
+    }
+
+    function start() {
+        if(!isGamerOver){
+            createPlatforms()
+            createDoodler()
+            setInterval(movePlatforms,30)
+            jump()
+        }
+    }
+    //attach to button
+    start()
+
+})
